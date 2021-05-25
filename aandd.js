498aa5f3c5193916088dedea3086a235953c9fb6
藤原紀香 <a8119169@aoyama.jp>
2021-05-25 19:07:38 +0900

    anddd.js

--- aandd.js
+++ aandd.js
@@ -10,3 +10,19 @@ function show(array) {
     //document.getElementById('res').innerHTML = html;
     document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
+
+function createSequentialNumbers(array, n) {
+    var n = Math.floor(Math.random() * 100) + 1;
+    for (var v = 1; v < n; v++) {
+        array.push(v);
+    }
+}
+
+unction createRandomNumbers(array, n) {
+    for (var v = 1; v < n; v++) {
+        for (var i = 0; i < n; i++) {
+            ran = Math.floor(Math.random() * 100) + 1;
+            array.push(ran);
+        }
+    }
+}
